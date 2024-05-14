48683c1c3aa8e6747478ecc7b1164ceaafbb0ea9
shizhu2136546 <shizhu2136546@126.com>
2024-05-14 22:13:36 +0900

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,19 +8,7 @@
     systems,
     nixpkgs,
     ...
-  } @ inputs: let
-    yarn_overlay = final: prev: {
-        yarn = prev.yarn.overrideAttrs(finalAttrs: prevAttrs: {
-            # This is to make sure that yarn runs the correct node version
-            # https://github.com/NixOS/nixpkgs/issues/145634#issuecomment-1627476963
-            installPhase = prevAttrs.installPhase + ''
-                ln -fs $out/libexec/yarn/bin/yarn $out/bin/yarn
-                ln -fs $out/libexec/yarn/bin/yarn.js $out/bin/yarn.js
-                ln -fs $out/libexec/yarn/bin/yarn $out/bin/yarnpkg
-            '';
-        });
-    };
-
+ 
     # This gives us a central place to set the node version
     node_overlay = final: prev: {
         nodejs = prev.nodejs-18_x;
